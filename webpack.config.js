ba18790d6de5317bd1bb4ea3a6d7d2ae916198cf
Alessandro Lepri <alex.lepri84@gmail.com>
2020-08-11 16:19:00 +0100

    webpack created

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+const fs = require("fs");
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
+
+module.exports = {
+  entry: resolveApp("src/app.js"),
+  output: {
+    path: resolveApp("dist"),
+    filename: "bundle.js",
+    publicPath: "",
+  },
+  mode: "development",
+  devtool: "eval",
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg)$/,
+        use: ["file-loader"],
+      },
+      {
+        // look for .js or .jsx files
+        test: /\.(js|jsx)$/,
+        // in the `src` directory
+        include: resolveApp("src"),
+        exclude: /(node_modules)/,
+        use: {
+          // use babel for transpiling JavaScript files
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              modules: { auto: true },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve("src"),
+    hot: true,
+    open: true,
+    port: 8000,
+    watchContentBase: true,
+    historyApiFallback: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000",
+        secure: false,
+      },
+    },
+  },
+  plugins: [
+    new Dotenv(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx", ".css"],
+    modules: ["node_modules"],
+    alias: {
+      Components: path.resolve(resolveApp("src"), "components"),
+      Containers: path.resolve(resolveApp("src"), "containers"),
+      Utils: path.resolve(resolveApp("src"), "utils"),
+    },
+  },
+};
